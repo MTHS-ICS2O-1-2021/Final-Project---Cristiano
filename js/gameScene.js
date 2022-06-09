8932f1be61ab3a5ffcb037c536c74beb68087ad7
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-06-09 17:00:03 +0000

    Heavy changes Added a GUI at the bottom that has text in it that tracks how many times you lost a that level Added two functions that creates a series of boxes on the x or y axis, and the ability to skip a box from being created

--- js/gameScene.js
+++ js/gameScene.js
@@ -15,7 +15,38 @@ class GameScene extends Phaser.Scene {
   addBox(boxX, boxY) {
     const box = this.physics.add.sprite(boxX, boxY, "boxImage").setScale(2.0)
     box
+
     this.boxGroup.add(box)
+  }
+
+  /**
+   * Adds dangerous boxes in a line on the x axis
+   */
+  addBoxLoopX(boxX, numberOfBoxes, skippedBoxes) {
+    var yLimit = 100 + numberOfBoxes * 200
+
+    for (let count = 100; count != yLimit; count = count + 200) {
+      if (skippedBoxes > 0) {
+        skippedBoxes = skippedBoxes - 1
+      } else {
+        this.addBox(boxX, count)
+      }
+    }
+  }
+
+  /**
+   * Adds dangerous boxes in a line on the y axis
+   */
+  addBoxLoopY(boxY, numberOfBoxes, skippedBoxes) {
+    var xLimit = 100 + numberOfBoxes * 200
+
+    for (let count = 100; count != xLimit; count = count + 200) {
+      if (skippedBoxes > 0) {
+        skippedBoxes = skippedBoxes - 1
+      } else {
+        this.addBox(count, boxY)
+      }
+    }
   }
 
   /**
@@ -25,6 +56,14 @@ class GameScene extends Phaser.Scene {
     super({ key: "gameScene" })
 
     this.player = null
+    this.timesLost = null
+    this.bottomGui = null
+    this.loseText = null
+    this.loseTextStyle = {
+      font: "50px Arial",
+      fill: "#ffffff",
+      align: "center",
+    }
   }
 
   /**
@@ -42,25 +81,32 @@ class GameScene extends Phaser.Scene {
 
     this.load.image("playerImage", "assets/playerImage.png")
     this.load.image("boxImage", "assets/box.png")
+    this.load.image("bottomGui", "assets/bottomGUI.png")
   }
 
   /**
    * Creates varibles and functions
    */
   create(data) {
-    this.player = this.physics.add.sprite(300, 50, "playerImage")
+    // Set times lost
+    this.timesLost = 0
+    // Add Player
+    this.player = this.physics.add.sprite(300, 100, "playerImage")
+    // Generate level one
     this.boxGroup = this.add.group()
-    this.addBox(100, 100)
-    this.addBox(100, 300)
-    this.addBox(100, 500)
-    this.addBox(100, 700)
-    this.addBox(500, 100)
-
-    // Collisions
+    this.addBoxLoopX(100, 4)
+    this.addBoxLoopX(500, 3)
+    this.addBoxLoopY(900, 4, 2)
+    // Add Gui
+    this.bottomGui = this.physics.add.sprite(0, 1197, "bottomGui").setScale(4.0)
+    this.loseText = this.add.text(20, 1015, "Times lost: " + this.timesLost, this.loseTextStyle)
+    // Set collision function
     this.physics.add.collider(
       this.player,
       this.boxGroup,
       function (playerCollide, boxCollide) {
+        this.timesLost++
+        this.loseText.text = "Times lost: " + this.timesLost
         playerCollide.x = 300
         playerCollide.y = 50
       }.bind(this)
@@ -71,12 +117,11 @@ class GameScene extends Phaser.Scene {
    * Updates every milisecond
    */
   update(time, delta) {
-    // Arrow Keys
+    // Movement
     const keyUpArrow = this.input.keyboard.addKey("UP")
     const keyDownArrow = this.input.keyboard.addKey("DOWN")
     const keyLeftArrow = this.input.keyboard.addKey("LEFT")
     const keyRightArrow = this.input.keyboard.addKey("RIGHT")
-    // WASD
     const keyW = this.input.keyboard.addKey("W")
     const keyS = this.input.keyboard.addKey("S")
     const keyA = this.input.keyboard.addKey("A")
@@ -84,29 +129,29 @@ class GameScene extends Phaser.Scene {
 
     if (keyUpArrow.isDown || keyW.isDown === true) {
       this.player.y -= 10
-      if (this.player.y < 0) {
-        this.player.y = 0
+      if (this.player.y < 50) {
+        this.player.y = 50
       }
     }
 
     if (keyDownArrow.isDown || keyS.isDown === true) {
       this.player.y += 10
-      if (this.player.y > 1080) {
-        this.player.y = 1080
+      if (this.player.y > 950) {
+        this.player.y = 950
       }
     }
 
     if (keyLeftArrow.isDown || keyA.isDown === true) {
       this.player.x -= 10
-      if (this.player.x < 0) {
-        this.player.x = 0
+      if (this.player.x < 50) {
+        this.player.x = 50
       }
     }
 
     if (keyRightArrow.isDown || keyD.isDown === true) {
       this.player.x += 10
-      if (this.player.x > 1920) {
-        this.player.x = 1920
+      if (this.player.x > 1870) {
+        this.player.x = 1870
       }
     }
   }
